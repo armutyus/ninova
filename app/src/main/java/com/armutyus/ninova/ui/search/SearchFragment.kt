e357f1b99eec1b4be8fee980385d003550ce9ce0
armutyus <ofdelibas@gmail.com>
2022-05-31 15:30:44 +0300

    search feature works

--- app/src/main/java/com/armutyus/ninova/ui/search/SearchFragment.kt
+++ app/src/main/java/com/armutyus/ninova/ui/search/SearchFragment.kt
@@ -1,13 +1,17 @@
 package com.armutyus.ninova.ui.search
 
+import android.app.ActionBar
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.armutyus.ninova.R
 import com.armutyus.ninova.databinding.FragmentSearchBinding
 import com.armutyus.ninova.databinding.FragmentShelvesBinding
 import com.armutyus.ninova.ui.shelves.ShelvesViewModel
+import com.lapism.search.widget.MaterialSearchView
+import com.lapism.search.widget.NavigationIconCompat
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
 
