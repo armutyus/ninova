374a74d210853b35a27c616b2b4fbbc369e64699
HasimD <delibasahmet@gmail.com>
2022-08-12 14:47:06 +0300

    Dispatcher on repo functions

--- app/src/main/java/com/armutyus/ninova/repository/BooksRepositoryImpl.kt
+++ app/src/main/java/com/armutyus/ninova/repository/BooksRepositoryImpl.kt
@@ -7,28 +7,34 @@ import com.armutyus.ninova.roomdb.NinovaDao
 import com.armutyus.ninova.roomdb.entities.BookShelfCrossRef
 import com.armutyus.ninova.roomdb.entities.BookWithShelves
 import com.armutyus.ninova.service.GoogleBooksApiService
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class BooksRepositoryImpl @Inject constructor(
     private val ninovaDao: NinovaDao,
-    private val googleBooksApiService: GoogleBooksApiService
+    private val googleBooksApiService: GoogleBooksApiService,
+    private val coroutineContext: CoroutineDispatcher = Dispatchers.IO
 ) : BooksRepositoryInterface {
 
-    override suspend fun getBookDetails(bookId: String): Flow<Response<BookDetails>> = flow {
-        try {
-            emit(Response.Loading)
-            val response = googleBooksApiService.getBookDetails(bookId)
-            if (response.isSuccessful) {
-                response.body()?.let {
-                    return@let emit(Response.Success(it))
-                } ?: emit(Response.Failure("Something went wrong!"))
-            } else {
-                emit(Response.Failure("Something went wrong!"))
+    override suspend fun getBookDetails(bookId: String): Flow<Response<BookDetails>> = withContext(coroutineContext) {
+        flow {
+            try {
+                emit(Response.Loading)
+                val response = googleBooksApiService.getBookDetails(bookId)
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        return@let emit(Response.Success(it))
+                    } ?: emit(Response.Failure("Something went wrong!"))
+                } else {
+                    emit(Response.Failure("Something went wrong!"))
+                }
+            } catch (e: Exception) {
+                emit(Response.Failure("Error: ${e.localizedMessage}"))
             }
-        } catch (e: Exception) {
-            emit(Response.Failure("Error: ${e.localizedMessage}"))
         }
     }
 
